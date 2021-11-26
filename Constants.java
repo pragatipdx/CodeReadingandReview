0043b3bf85f783170617225fad4c68cb58498faf
kirtan6004 <kirtanpatel209@gmail.com>
2021-11-26 14:27:35 -0800

    added comment

--- Constants.java
+++ Constants.java
@@ -1,6 +1,5 @@
-
-public class Constants
+public class Constants // constant class to find the location
 {
-    public static final String IP_ADDRESS = "102.130.64.103";
+    public static final String IP_ADDRESS = "102.130.64.103"; // enter the IP to find the location
     public static boolean enableFileWatch = false;
 }
