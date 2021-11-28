0513ac2b31d30c581782f8738176c39390901a42
kirtan6004 <kirtanpatel209@gmail.com>
2021-11-28 12:24:39 -0800

    properly formatted the code with brackets and spacing

--- IPExt.java
+++ IPExt.java
@@ -35,7 +35,8 @@ public class IPExt {
         }
     }
 
-    public static String[] find(String ip) {
+    public static String[] find(String ip)
+    {
         String[] ips = ip.split("\\.");
         int prefix_value = (Integer.valueOf(ips[0]) * 256 + Integer.valueOf(ips[1]));
         long ip2long_value = ip2long(ip);
@@ -45,9 +46,11 @@ public class IPExt {
         long index_offset = -1;
         int index_length = -1;
         byte b = 0;
-        for (start = start * 9 + 262144; start < max_comp_len; start += 9) {
+        for (start = start * 9 + 262144; start < max_comp_len; start += 9)
+        {
             tmpInt = int2long(indexBuffer.getInt(start));
-            if (tmpInt >= ip2long_value) {
+            if (tmpInt >= ip2long_value)
+            {
                 index_offset = bytesToLong(b, indexBuffer.get(start + 6), indexBuffer.get(start + 5), indexBuffer.get(start + 4));
                 index_length = (0xFF & indexBuffer.get(start + 7) << 8) + (0xFF & indexBuffer.get(start + 8));
                 break;
@@ -57,33 +60,42 @@ public class IPExt {
         byte[] areaBytes;
 
         lock.lock();
-        try {
+        try
+        {
             dataBuffer.position(offset + (int) index_offset - 262144);
             areaBytes = new byte[index_length];
             dataBuffer.get(areaBytes, 0, index_length);
-        } finally {
+        }
+        finally
+        {
             lock.unlock();
         }
 
         return new String(areaBytes).split("\t");
     }
 
-    private static void watch() {
-        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+    private static void watch()
+    {
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 long time = ipFile.lastModified();
-                if (time > lastModifyTime) {
+                if (time > lastModifyTime)
+                {
                     load();
                 }
             }
         }, 1000L, 5000L, TimeUnit.MILLISECONDS);
     }
 
-    private static void load() {
+    private static void load()
+    {
         lastModifyTime = ipFile.lastModified();
         lock.lock();
-        try {
+        try
+        {
             dataBuffer = ByteBuffer.wrap(getBytesByFile(ipFile));
             dataBuffer.position(0);
             offset = dataBuffer.getInt(); // indexLength
@@ -92,36 +104,51 @@ public class IPExt {
             indexBuffer = ByteBuffer.wrap(indexBytes);
             indexBuffer.order(ByteOrder.LITTLE_ENDIAN);
 
-            for (int i = 0; i < 256; i++) {
-                for (int j = 0; j < 256; j++) {
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
                     index[i * 256 + j] = indexBuffer.getInt();
                 }
             }
             indexBuffer.order(ByteOrder.BIG_ENDIAN);
-        } finally {
+        }
+        finally
+        {
             lock.unlock();
         }
     }
 
-    private static byte[] getBytesByFile(File file) {
+    private static byte[] getBytesByFile(File file)
+    {
         FileInputStream fin = null;
         byte[] bs = new byte[new Long(file.length()).intValue()];
-        try {
+        try
+        {
             fin = new FileInputStream(file);
             int readBytesLength = 0;
             int i;
-            while ((i = fin.available()) > 0) {
+            while ((i = fin.available()) > 0)
+            {
                 fin.read(bs, readBytesLength, i);
                 readBytesLength += i;
             }
-        } catch (IOException ioe) {
+        }
+        catch (IOException ioe)
+        {
             ioe.printStackTrace();
-        } finally {
-            try {
-                if (fin != null) {
+        }
+        finally
+        {
+            try
+            {
+                if (fin != null)
+                {
                     fin.close();
                 }
-            } catch (IOException e){
+            }
+            catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
@@ -129,7 +156,8 @@ public class IPExt {
         return bs;
     }
 
-    private static long bytesToLong(byte a, byte b, byte c, byte d) {
+    private static long bytesToLong(byte a, byte b, byte c, byte d)
+    {
         return int2long((((a & 0xff) << 24) | ((b & 0xff) << 16) | ((c & 0xff) << 8) | (d & 0xff)));
     }
     private static int str2Ip(String ip)
