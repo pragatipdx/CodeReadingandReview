8893f93093c1770529af98bca691157ac8c9d333
Kirtan6004 <77852338+Kirtan6004@users.noreply.github.com>
2021-12-03 22:22:28 -0800

    Update Constants.java
    comments

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,6 +1,6 @@
-public class Constants 
+public class Constants  // seperate file for all constants
 {
-    public static final String IP_ADDRESS = "213.255.193.25";
+    public static final String IP_ADDRESS = "213.255.193.25"; // enter the IP for which location needs to be found
     public static boolean enableFileWatch = false;
 }
 
